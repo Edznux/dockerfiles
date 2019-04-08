f6be4c00454e20e3aaa2e87b4aac3f1ddf55c047
Edznux <edznux@gmail.com>
2019-04-08 22:56:06 +0200

    Add single build check

--- build-all.sh
+++ build-all.sh
@@ -3,7 +3,22 @@
 source DOCKER_REPO
 
 DOCKERFILES_DIR=`find . -maxdepth 1 -type d | grep -v "\./\." | grep -v "^\.$"`
-for img in $DOCKERFILES_DIR; do
+
+build(){
+	img="$1"
 	echo "docker build -t $DOCKER_REPO_PREFIX/$(basename $img) $img/"
 	docker build -t $DOCKER_REPO_PREFIX/$(basename $img) $img/
-done
+}
+
+if [ $# -eq 1 ]; then
+	echo "Yes, 1"
+	build "$1"
+else
+	echo "Building all !"
+	for img in $DOCKERFILES_DIR; do
+		build "$img"
+	done
+fi
+
+
+

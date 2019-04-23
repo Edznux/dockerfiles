5a88164b93ba370443a9667931ba6178278cbfed
Edznux <edznux@gmail.com>
2019-04-23 13:31:27 +0200

    Fix : Remove sourcing of . and .git from other directories

--- source-all.sh
+++ source-all.sh
@@ -3,8 +3,8 @@
 DIR=`dirname "$0"`
 source $DIR/DOCKER_REPO
 
-DOCKERFILES_DIR=`find $DIR/ -maxdepth 1 -type d | grep -v "\./\." | grep -v "^\.$"` 
+DOCKERFILES_DIR=`find $DIR -maxdepth 1 -type d | grep -v "\./\." | grep -v "^$DIR$" | grep -v "\.git"` 
 for img in $DOCKERFILES_DIR; do
-	. $DIR/$img/alias.sh
+	. $img/alias.sh
 done
 

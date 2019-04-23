5c10696438deed233fc330c77900e0f7f3e37407
Edznux <edznux@gmail.com>
2019-04-23 13:45:31 +0200

    FFS, hardcode ~/dockerfiles dir

--- source-all.sh
+++ source-all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-DIR=`dirname "$0"`
+# DIR=`dirname "$0"` # TOFIX
+DIR="$HOME/dockerfiles/" # TOFIX
 source $DIR/DOCKER_REPO
 
 DOCKERFILES_DIR=`find $DIR -maxdepth 1 -type d | grep -v "\./\." | grep -v "^$DIR$" | grep -v "\.git"` 
